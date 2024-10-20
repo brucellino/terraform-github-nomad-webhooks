99d0ebe625eca8025d433d4214943733440532ab
Bruce Becker <brucellino@protonmail.ch>
2024-10-20 16:10:25 +0200

    chore: bump terraform and provider versions
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,3 @@
-# Use this file to declare the terraform configuration
-# Add things like:
-# - required version
-# - required providers
-# Do not add things like:
-# - provider configuration
-# - backend configuration
-# These will be declared in the terraform document which consumes the module.
-
 terraform {
   required_version = ">1.6.0"
   required_providers {
