4d3705b1c37ee7f6675896b9d21f297806e0f500
Bruce Becker <brucellino@protonmail.ch>
2024-10-20 16:10:57 +0200

    build: add exmaple for org
    Signed-off-by: Bruce Becker <brucellino@protonmail.ch>

---
+++ examples/org/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_version = "~> 1.9"
+  backend "consul" {
+    path = "terraform_github_nomad_webhooks/org"
+  }
+  required_providers {
+    # We need github to provide access to github
+    github = {
+      source  = "integrations/github"
+      version = "~> 6"
+    }
+    # we're going to need vault to read and write secrets
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4"
+    }
+    # Cloudflare will be used to create a few
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4"
+    }
+    nomad = {
+      source  = "hashicorp/nomad"
+      version = "~> 2"
+    }
+  }
+}
+
+variable "domain" {
+  description = "The domain you will be deploying to. You must already own this domain."
+  default     = "brucellino.dev"
+  type        = string
+}
+
+variable "secrets_mount" {
+  type        = string
+  description = "Name of the vault mount where the github secrets are kept."
+  default     = "hashiatho.me-v2"
+}
+
+provider "vault" {}
+# Use vault to get the secrets for configuring the other providers
+data "vault_kv_secret_v2" "github" {
+  mount = var.secrets_mount
+  name  = "github"
+}
+
+data "vault_kv_secret_v2" "cloudflare" {
+  mount = "cloudflare"
+  name  = var.domain
+}
+
+provider "cloudflare" {
+  api_token = data.vault_kv_secret_v2.cloudflare.data.github_runner_token
+}
+
+provider "github" {
+  token = data.vault_kv_secret_v2.github.data.gh_token
+  owner = "hashi-at-home"
+  alias = "hah"
+}
+
+provider "nomad" {}
+
+moved {
+  from = module.example
+  to   = module.mine
+}
+module "hah" {
+  providers = {
+    github = github.hah
+  }
+  source            = "../../"
+  github_username   = "hashi-at-home"
+  org               = true
+  include_archived  = false
+  cloudflare_domain = var.domain
+}
